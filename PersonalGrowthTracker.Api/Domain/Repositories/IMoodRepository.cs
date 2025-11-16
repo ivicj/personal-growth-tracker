4feb2415515c414f85c02164be15ab7f109b1a5b
ivicj <ivicj123@gmail.com>
2025-11-16 20:51:54 +0100

    add domain entity and in memory repository

---
+++ PersonalGrowthTracker.Api/Domain/Repositories/IMoodRepository.cs
@@ -0,0 +1,11 @@
+﻿using PersonalGrowthTracker.Api.Domain.Entities;
+
+namespace PersonalGrowthTracker.Api.Domain.Repositories
+{
+    public interface IMoodRepository
+    {
+        IEnumerable<MoodEntry> GetAll();
+        MoodEntry? GetById(int id);
+        MoodEntry Add(MoodEntry entry);
+    }
+}
